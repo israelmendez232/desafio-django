3834ff35177c4bd51f7c644e2f5aa4ddefb580ca
Israel Silva <israel.silva@ESTRATEGIA.NET>
2019-10-10 09:58:51 -0300

    Atualizando usuários.

--- evestibulares/settings.py
+++ evestibulares/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'evestibulares.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
